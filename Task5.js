f0909be4cb70e8fde71ec51bee12fa65c36b0ef1
rehan.raees <rehan.raees1@gmail.com>
2023-10-01 16:38:11 +0500

    Task complete

---
+++ Task5.js
@@ -0,0 +1,5 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message
+Object.defineProperty(exports, "__esModule", { value: true });
+let person_name = "Rehan Raees";
+console.log(`"A person who never made a misktake never tried anything new." - ${person_name}`);
---
+++ Task5.ts
@@ -0,0 +1,9 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message
+
+let person_name="Rehan Raees"
+console.log(`"A person who never made a misktake never tried anything new." - ${person_name}`)
+
+
+export{}
+
+
