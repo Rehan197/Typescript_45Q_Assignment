bd29c122f5492e7d1cd46c658a800399bcd6b5c5
rehan.raees <rehan.raees1@gmail.com>
2023-10-01 14:19:31 +0500

    Completed

--- task3.js
+++ task3.js
@@ -1,12 +1,12 @@
 "use strict";
 //Q3-Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 Object.defineProperty(exports, "__esModule", { value: true });
-var Myname = "Rehan Raees";
-var TitleCase = "";
+let Myname = "Rehan Raees";
+let TitleCase = "";
 console.log(Myname.toLocaleUpperCase());
 console.log(Myname.toLowerCase());
 console.log(Myname.charAt(0).toUpperCase() + Myname.slice(1).toLowerCase());
-for (var i = 0; i < Myname.length; i++) {
+for (let i = 0; i < Myname.length; i++) {
     if (i === 0 || Myname[i - 1] === " ") {
         TitleCase += Myname[i].toUpperCase();
     }
